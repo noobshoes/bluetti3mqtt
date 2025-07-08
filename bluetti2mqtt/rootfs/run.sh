580cf6ebe7c2c46b084040cf1b5edb34abad5d42
Kyle Sorensen <107611427+noobshoes@users.noreply.github.com>
2025-07-08 13:26:37 +1000

    Update run.sh

--- bluetti2mqtt/rootfs/run.sh
+++ bluetti2mqtt/rootfs/run.sh
@@ -14,76 +14,76 @@ SCAN=$(bashio::config 'scan')
 
 # Setup MQTT Auto-Configuration if values are not set.
 if bashio::config.has_value 'mqtt_host'; then
-	MQTT_HOST=$(bashio::config 'mqtt_host')
+    MQTT_HOST=$(bashio::config 'mqtt_host')
 else
-	MQTT_HOST=$(bashio::services "mqtt" "host")
+    MQTT_HOST=$(bashio::services "mqtt" "host")
 fi
 
 if bashio::config.has_value 'mqtt_port'; then
-	MQTT_PORT=$(bashio::config 'mqtt_port')
+    MQTT_PORT=$(bashio::config 'mqtt_port')
 else
-	MQTT_PORT=$(bashio::services "mqtt" "port")
+    MQTT_PORT=$(bashio::services "mqtt" "port")
 fi
 
 if bashio::config.has_value 'mqtt_username'; then
-	MQTT_USERNAME=$(bashio::config 'mqtt_username')
+    MQTT_USERNAME=$(bashio::config 'mqtt_username')
 else
-	MQTT_USERNAME=$(bashio::services mqtt "username")
+    MQTT_USERNAME=$(bashio::services mqtt "username")
 fi
 
 if bashio::config.has_value 'mqtt_password'; then
-	MQTT_PASSWORD=$(bashio::config 'mqtt_password')
+    MQTT_PASSWORD=$(bashio::config 'mqtt_password')
 else
-	MQTT_PASSWORD=$(bashio::services "mqtt" "password")
+    MQTT_PASSWORD=$(bashio::services "mqtt" "password")
 fi
 
 if [ $(bashio::config 'debug') == true ]; then
-	export DEBUG=true
-	bashio::log.info 'Debug mode is enabled.'
+    export DEBUG=true
+    bashio::log.info 'Debug mode is enabled.'
 fi
 
 args=()
 if [ ${SCAN} == true ]; then
-	args+=(--scan)
+    args+=(--scan)
 fi
 
 case $MODE in
 
-	mqtt)
-		bashio::log.info 'Starting bluetti-mqtt...'
-		args+=(--broker "${MQTT_HOST}" --port "${MQTT_PORT}")
-		if [[ -n "${MQTT_USERNAME}" ]]; then
-			args+=(--username "${MQTT_USERNAME}")
-		fi
-		if [[ -n "${MQTT_PASSWORD}" ]]; then
-			args+=(--password "${MQTT_PASSWORD}")
-		fi
-		args+=(--interval "${POLL_SEC}" --ha-config "${HA_CONFIG}" "${BT_MAC}")
-		bluetti-mqtt "${args[@]}"
-		;;
+    mqtt)
+        bashio::log.info 'Starting bluetti-mqtt...'
+        args+=(--broker "${MQTT_HOST}" --port "${MQTT_PORT}")
+        if [[ -n "${MQTT_USERNAME}" ]]; then
+            args+=(--username "${MQTT_USERNAME}")
+        fi
+        if [[ -n "${MQTT_PASSWORD}" ]]; then
+            args+=(--password "${MQTT_PASSWORD}")
+        fi
+        args+=(--interval "${POLL_SEC}" --ha-config "${HA_CONFIG}" "${BT_MAC}")
+        bluetti-mqtt "${args[@]}"
+        ;;
 
-	discovery)
-		bashio::log.info 'Starting bluetti-discovery...'
-		bashio::log.info 'Messages are NOT published to the MQTT broker in discovery mode.'
-		mkdir -p /share/bluetti2mqtt/
-		args+=( \
-			--log /share/bluetti2mqtt/discovery_$(date "+%m%d%y%H%M%S").log \
-			${BT_MAC})
-		bluetti-discovery ${args[@]}
-		;;
+    discovery)
+        bashio::log.info 'Starting bluetti-discovery...'
+        bashio::log.info 'Messages are NOT published to the MQTT broker in discovery mode.'
+        mkdir -p /share/bluetti2mqtt/
+        args+=( \
+            --log /share/bluetti2mqtt/discovery_$(date "+%m%d%y%H%M%S").log \
+            ${BT_MAC})
+        bluetti-discovery ${args[@]}
+        ;;
 
-	logger)
-		bashio::log.info 'Starting bluetti-logger...'
-		bashio::log.info 'Messages are NOT published to the MQTT broker in logger mode.'
-		mkdir -p /share/bluetti2mqtt/
-		args+=( \
-			--log /share/bluetti2mqtt/logger_$(date "+%m%d%y%H%M%S").log \
-			${BT_MAC})
-		bluetti-logger ${args[@]}
-		;;
+    logger)
+        bashio::log.info 'Starting bluetti-logger...'
+        bashio::log.info 'Messages are NOT published to the MQTT broker in logger mode.'
+        mkdir -p /share/bluetti2mqtt/
+        args+=( \
+            --log /share/bluetti2mqtt/logger_$(date "+%m%d%y%H%M%S").log \
+            ${BT_MAC})
+        bluetti-logger ${args[@]}
+        ;;
 
-	*)
-		bashio::log.warning "No mode selected!  Please choose either 'mqtt', 'discovery', or 'logger'."
-		;;
+    *)
+        bashio::log.warning "No mode selected!  Please choose either 'mqtt', 'discovery', or 'logger'."
+        ;;
 
 esac
